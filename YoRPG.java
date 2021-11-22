875fb8240ca74c3caf172604658ec6d268c9d1bb
Salaj Rijal <srijal30@stuy.edu>
2021-11-22 13:17:53 -0500

    yo

--- YoRPG.java
+++ YoRPG.java
@@ -2,18 +2,18 @@
  * class YoRPG -- Driver file for Ye Olde Role Playing Game.
  * Simulates monster encounters of a wandering adventurer.
  * Required classes: Protagonist, Monster
- * 
+ *
  * USAGE:
  * Compile. Note messages generated.
  * Devise a plan of attack with your trio.
  * Code incrementally, testing each bit of new functionality as you go.
  * The only modification you should make to this driver file is moving comment bar down in main method, and filling in DISCO/QCC
  * (If you feel other changes are merited, note what and why, so that we may discuss on the 'morrow.)
- * 
+ *
  * DISCO:
  *
  * QCC:
- * 
+ *
  **********************************************/
 
 import java.io.*;
@@ -27,8 +27,8 @@ public class YoRPG {
   public final static int MAX_ENCOUNTERS = 5;
 
   //each round, a Protagonist and a Monster will be instantiated...
-  private Protagonist pat;   
-  private Monster smaug;     
+  private Protagonist pat;
+  private Monster smaug;
 
   //Game Vars
   private int moveCount;
@@ -52,13 +52,13 @@ public class YoRPG {
   // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-  
+
   // ~~~~~~~~~~~~~~ METHODS ~~~~~~~~~~~~~~~~~~~
 
   /*=============================================
     void newGame() -- gathers info to begin a new game
-    pre:  
-    post: according to user input, modifies instance var for difficulty 
+    pre:
+    post: according to user input, modifies instance var for difficulty
     and instantiates a Protagonist
     =============================================*/
   public void newGame() {
@@ -111,7 +111,7 @@ public class YoRPG {
 
     if ( Math.random() >= ( difficulty / 3.0 ) )
 	    System.out.println( "\nNothing to see here. Move along!" );
-    
+
       else {
 	    System.out.println( "\nLo, yonder monster approacheth!" );
 
@@ -150,7 +150,7 @@ public class YoRPG {
 
 	    //option 1: you & the monster perish
 	    if ( !smaug.isAlive() && !pat.isAlive() ) {
-        System.out.println( "'Twas an epic battle, to be sure... " + 
+        System.out.println( "'Twas an epic battle, to be sure... " +
                             "You cut ye olde monster down, but " +
                             "with its dying breath ye olde monster. " +
                             "laid a fatal blow upon thy skull." );
@@ -174,14 +174,13 @@ public class YoRPG {
 
 
   public static void main( String[] args ) {
-    //As usual, move the begin-comment bar down as you progressively 
+    //As usual, move the begin-comment bar down as you progressively
     //test each new bit of functionality...
-
     //loading...
     YoRPG game = new YoRPG();
-    
+
     int encounters = 0;
-    
+
     while( encounters < MAX_ENCOUNTERS ) {
       if ( !game.playTurn() ){
         break;
@@ -189,7 +188,7 @@ public class YoRPG {
       encounters++;
       System.out.println();
     }
-    
+
     System.out.println( "Thy game doth be over." );
     /*================================================
 	  ================================================*/
