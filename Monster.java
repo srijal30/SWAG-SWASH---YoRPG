676cb12d9e9d2dd54e6fc534cd689cf85670acc6
Michael Kamela <mkamela20@stuy.edu>
2021-11-28 18:55:26 -0500

    deletions

--- Monster.java
+++
@@ -1,8 +0,0 @@
-public class Monster extends Character {
-  public Monster(){
-    //setHealth( 40 + (int) (Math.random() * 30)) ; 
-    //setStrength( 10 + (int) (Math.random() * 5)) ;
-    //setDefense( 10 + (int) (Math.random() * 5)) ;
-  }
-
-}
